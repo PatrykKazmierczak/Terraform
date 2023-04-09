a7977b9f2ad588ea15b737341542fb709f345a7c
PatrykKazmierczak <patrykkazmierczak0@gmail.com>
2023-04-09 11:05:37 +0200

    update

--- Modules/terraform-aws-s3-bucket/README.md
+++ Modules/terraform-aws-s3-bucket/README.md
@@ -1,57 +1,63 @@
-Complete S3 bucket with most of supported features enabled
-Configuration in this directory creates S3 bucket which demos such capabilities:
+# S3 bucket object
 
-static web-site hosting
-access logging (for S3, ELB and ALB/NLB)
-versioning
-CORS
-lifecycle rules
-server-side encryption
-object locking
-grants (required for CloudFront logs)
-Please check S3 replication example to see Cross-Region Replication (CRR) supported by this module.
+Configuration in this directory creates S3 bucket objects with different configurations.
 
-Usage
+## Usage
+
 To run this example you need to execute:
 
+```bash
 $ terraform init
 $ terraform plan
 $ terraform apply
-Note that this example may create resources which cost money. Run terraform destroy when you don't need these resources.
+```
 
-Requirements
-Name	Version
-terraform	>= 0.13.1
-aws	>= 4.9
-random	>= 2.0
-Providers
-Name	Version
-aws	>= 4.9
-random	>= 2.0
-Modules
-Name	Source	Version
-cloudfront_log_bucket	../../	n/a
-log_bucket	../../	n/a
-s3_bucket	../../	n/a
-Resources
-Name	Type
-aws_iam_role.this	resource
-aws_kms_key.objects	resource
-random_pet.this	resource
-aws_caller_identity.current	data source
-aws_canonical_user_id.current	data source
-aws_cloudfront_log_delivery_canonical_user_id.cloudfront	data source
-aws_iam_policy_document.bucket_policy	data source
-Inputs
+Note that this example may create resources which cost money. Run `terraform destroy` when you don't need these resources.
+
+<!-- BEGINNING OF PRE-COMMIT-TERRAFORM DOCS HOOK -->
+## Requirements
+
+| Name | Version |
+|------|---------|
+| <a name="requirement_terraform"></a> [terraform](#requirement\_terraform) | >= 0.13.1 |
+| <a name="requirement_aws"></a> [aws](#requirement\_aws) | >= 4.9 |
+| <a name="requirement_random"></a> [random](#requirement\_random) | >= 2.0 |
+
+## Providers
+
+| Name | Version |
+|------|---------|
+| <a name="provider_aws"></a> [aws](#provider\_aws) | >= 4.9 |
+| <a name="provider_random"></a> [random](#provider\_random) | >= 2.0 |
+
+## Modules
+
+| Name | Source | Version |
+|------|--------|---------|
+| <a name="module_object"></a> [object](#module\_object) | ../../modules/object | n/a |
+| <a name="module_object_complete"></a> [object\_complete](#module\_object\_complete) | ../../modules/object | n/a |
+| <a name="module_object_locked"></a> [object\_locked](#module\_object\_locked) | ../../modules/object | n/a |
+| <a name="module_s3_bucket"></a> [s3\_bucket](#module\_s3\_bucket) | ../../ | n/a |
+| <a name="module_s3_bucket_with_object_lock"></a> [s3\_bucket\_with\_object\_lock](#module\_s3\_bucket\_with\_object\_lock) | ../../ | n/a |
+
+## Resources
+
+| Name | Type |
+|------|------|
+| [aws_kms_key.this](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/kms_key) | resource |
+| [random_pet.this](https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/pet) | resource |
+
+## Inputs
+
 No inputs.
 
-Outputs
-Name	Description
-s3_bucket_arn	The ARN of the bucket. Will be of format arn:aws:s3:::bucketname.
-s3_bucket_bucket_domain_name	The bucket domain name. Will be of format bucketname.s3.amazonaws.com.
-s3_bucket_bucket_regional_domain_name	The bucket region-specific domain name. The bucket domain name including the region name, please refer here for format. Note: The AWS CloudFront allows specifying S3 region-specific endpoint when creating S3 origin, it will prevent redirect issues from CloudFront to S3 Origin URL.
-s3_bucket_hosted_zone_id	The Route 53 Hosted Zone ID for this bucket's region.
-s3_bucket_id	The name of the bucket.
-s3_bucket_region	The AWS region this bucket resides in.
-s3_bucket_website_domain	The domain of the website endpoint, if the bucket is configured with a website. If not, this will be an empty string. This is used to create Route 53 alias records.
-s3_bucket_website_endpoint	The website endpoint, if the bucket is configured with a website. If not, this will be an empty string.
+## Outputs
+
+| Name | Description |
+|------|-------------|
+| <a name="output_s3_bucket_arn"></a> [s3\_bucket\_arn](#output\_s3\_bucket\_arn) | The ARN of the bucket. Will be of format arn:aws:s3:::bucketname. |
+| <a name="output_s3_bucket_id"></a> [s3\_bucket\_id](#output\_s3\_bucket\_id) | The name of the bucket. |
+| <a name="output_s3_object_etag"></a> [s3\_object\_etag](#output\_s3\_object\_etag) | The ETag generated for the object (an MD5 sum of the object content). |
+| <a name="output_s3_object_id"></a> [s3\_object\_id](#output\_s3\_object\_id) | The key of S3 object |
+| <a name="output_s3_object_version_id"></a> [s3\_object\_version\_id](#output\_s3\_object\_version\_id) | A unique version ID value for the object, if bucket versioning is enabled. |
+<!-- END OF PRE-COMMIT-TERRAFORM DOCS HOOK -->
--- Modules/terraform-aws-s3-bucket/main.tf
+++ Modules/terraform-aws-s3-bucket/main.tf
@@ -2,6 +2,7 @@ provider "aws" {
   region = local.region
 
   # Make it faster by skipping something
+  skip_get_ec2_platforms      = true
   skip_metadata_api_check     = true
   skip_region_validation      = true
   skip_credentials_validation = true
@@ -9,340 +10,95 @@ provider "aws" {
 }
 
 locals {
-  bucket_name = "s3-bucket-${random_pet.this.id}"
-  region      = "eu-west-1"
+  region = "eu-central-1"
 }
 
-data "aws_caller_identity" "current" {}
+module "object" {
+  source = "../../modules/object"
 
-data "aws_canonical_user_id" "current" {}
+  bucket = module.s3_bucket.s3_bucket_id
+  key    = "${random_pet.this.id}-local"
 
-data "aws_cloudfront_log_delivery_canonical_user_id" "cloudfront" {}
+  file_source = "README.md"
+  #  content = file("README.md")
+  #  content_base64 = filebase64("README.md")
 
+  tags = {
+    Sensitive = "not-really"
+  }
+}
+module "object_complete" {
+  source = "../../modules/object"
+
+  bucket = module.s3_bucket.s3_bucket_id
+  key    = "${random_pet.this.id}-complete"
+
+  content = jsonencode({ data : "value" })
+
+  acl           = "public-read"
+  storage_class = "ONEZONE_IA"
+  force_destroy = true
+
+  cache_control       = "public; max-age=1200"
+  content_disposition = "attachment; filename=\"invoice.pdf\""
+  content_encoding    = "gzip"
+  content_language    = "en-US"
+  content_type        = "application/json"
+
+  website_redirect = "https://www.google.com/"
+  metadata = {
+    key         = "value1"
+    another-key = "value2"
+  }
+
+  server_side_encryption = "aws:kms"
+  kms_key_id             = aws_kms_key.this.arn
+}
+
+module "object_locked" {
+  source = "../../modules/object"
+
+  bucket = module.s3_bucket_with_object_lock.s3_bucket_id
+  key    = "${random_pet.this.id}-locked"
+
+  content = "some-content-locked-by-governance"
+
+  force_destroy = true
+
+  object_lock_legal_hold_status = true # boolean or string ("ON" or "OFF")
+  object_lock_mode              = "GOVERNANCE"
+  object_lock_retain_until_date = formatdate("YYYY-MM-DD'T'hh:00:00Z", timeadd(timestamp(), "1h")) # some time in the future
+}
+
+##################
+# Extra resources
+##################
 resource "random_pet" "this" {
   length = 2
 }
 
-resource "aws_kms_key" "objects" {
-  description             = "KMS key is used to encrypt bucket objects"
+resource "aws_kms_key" "this" {
+  description             = "KMS key for S3 object"
   deletion_window_in_days = 7
 }
 
-resource "aws_iam_role" "this" {
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "ec2.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
-}
-EOF
-}
-
-data "aws_iam_policy_document" "bucket_policy" {
-  statement {
-    principals {
-      type        = "AWS"
-      identifiers = [aws_iam_role.this.arn]
-    }
-
-    actions = [
-      "s3:ListBucket",
-    ]
-
-    resources = [
-      "arn:aws:s3:::${local.bucket_name}",
-    ]
-  }
-}
-
-module "log_bucket" {
-  source = "../../"
-
-  bucket        = "logs-${random_pet.this.id}"
-  acl           = "log-delivery-write"
-  force_destroy = true
-
-  attach_elb_log_delivery_policy        = true
-  attach_lb_log_delivery_policy         = true
-  attach_deny_insecure_transport_policy = true
-  attach_require_latest_tls_policy      = true
-}
-
-module "cloudfront_log_bucket" {
-  source = "../../"
-
-  bucket = "cloudfront-logs-${random_pet.this.id}"
-
-  grant = [{
-    type       = "CanonicalUser"
-    permission = "FULL_CONTROL"
-    id         = data.aws_canonical_user_id.current.id
-    }, {
-    type       = "CanonicalUser"
-    permission = "FULL_CONTROL"
-    id         = data.aws_cloudfront_log_delivery_canonical_user_id.cloudfront.id # Ref. https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/AccessLogs.html
-    }
-  ]
-
-  owner = {
-    id = data.aws_canonical_user_id.current.id
-  }
-
-  force_destroy = true
-}
-
+#############
+# S3 buckets
+#############
 module "s3_bucket" {
   source = "../../"
 
-  bucket = local.bucket_name
+  bucket        = random_pet.this.id
+  force_destroy = true
+}
 
-  force_destroy       = true
-  acceleration_status = "Suspended"
-  request_payer       = "BucketOwner"
+module "s3_bucket_with_object_lock" {
+  source = "../../"
 
-  tags = {
-    Owner = "Anton"
-  }
+  bucket        = "${random_pet.this.id}-with-object-lock"
+  force_destroy = true
 
-  # Note: Object Lock configuration can be enabled only on new buckets
-  # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_object_lock_configuration
-  object_lock_enabled = true
   object_lock_configuration = {
-    rule = {
-      default_retention = {
-        mode = "GOVERNANCE"
-        days = 1
-      }
-    }
+    object_lock_enabled = "Enabled"
   }
-
-  # Bucket policies
-  attach_policy                         = true
-  policy                                = data.aws_iam_policy_document.bucket_policy.json
-  attach_deny_insecure_transport_policy = true
-  attach_require_latest_tls_policy      = true
-
-  # S3 bucket-level Public Access Block configuration
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-
-  # S3 Bucket Ownership Controls
-  # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_ownership_controls
-  control_object_ownership = true
-  object_ownership         = "BucketOwnerPreferred"
-
-  expected_bucket_owner = data.aws_caller_identity.current.account_id
-
-  acl = "private" # "acl" conflicts with "grant" and "owner"
-
-  logging = {
-    target_bucket = module.log_bucket.s3_bucket_id
-    target_prefix = "log/"
-  }
-
-  versioning = {
-    status     = true
-    mfa_delete = false
-  }
-
-  website = {
-    # conflicts with "error_document"
-    #        redirect_all_requests_to = {
-    #          host_name = "https://modules.tf"
-    #        }
-
-    index_document = "index.html"
-    error_document = "error.html"
-    routing_rules = [{
-      condition = {
-        key_prefix_equals = "docs/"
-      },
-      redirect = {
-        replace_key_prefix_with = "documents/"
-      }
-      }, {
-      condition = {
-        http_error_code_returned_equals = 404
-        key_prefix_equals               = "archive/"
-      },
-      redirect = {
-        host_name          = "archive.myhost.com"
-        http_redirect_code = 301
-        protocol           = "https"
-        replace_key_with   = "not_found.html"
-      }
-    }]
-  }
-
-  server_side_encryption_configuration = {
-    rule = {
-      apply_server_side_encryption_by_default = {
-        kms_master_key_id = aws_kms_key.objects.arn
-        sse_algorithm     = "aws:kms"
-      }
-    }
-  }
-
-  cors_rule = [
-    {
-      allowed_methods = ["PUT", "POST"]
-      allowed_origins = ["https://modules.tf", "https://terraform-aws-modules.modules.tf"]
-      allowed_headers = ["*"]
-      expose_headers  = ["ETag"]
-      max_age_seconds = 3000
-      }, {
-      allowed_methods = ["PUT"]
-      allowed_origins = ["https://example.com"]
-      allowed_headers = ["*"]
-      expose_headers  = ["ETag"]
-      max_age_seconds = 3000
-    }
-  ]
-
-  lifecycle_rule = [
-    {
-      id      = "log"
-      enabled = true
-
-      filter = {
-        tags = {
-          some    = "value"
-          another = "value2"
-        }
-      }
-
-      transition = [
-        {
-          days          = 30
-          storage_class = "ONEZONE_IA"
-          }, {
-          days          = 60
-          storage_class = "GLACIER"
-        }
-      ]
-
-      #        expiration = {
-      #          days = 90
-      #          expired_object_delete_marker = true
-      #        }
-
-      #        noncurrent_version_expiration = {
-      #          newer_noncurrent_versions = 5
-      #          days = 30
-      #        }
-    },
-    {
-      id                                     = "log1"
-      enabled                                = true
-      abort_incomplete_multipart_upload_days = 7
-
-      noncurrent_version_transition = [
-        {
-          days          = 30
-          storage_class = "STANDARD_IA"
-        },
-        {
-          days          = 60
-          storage_class = "ONEZONE_IA"
-        },
-        {
-          days          = 90
-          storage_class = "GLACIER"
-        },
-      ]
-
-      noncurrent_version_expiration = {
-        days = 300
-      }
-    },
-    {
-      id      = "log2"
-      enabled = true
-
-      filter = {
-        prefix                   = "log1/"
-        object_size_greater_than = 200000
-        object_size_less_than    = 500000
-        tags = {
-          some    = "value"
-          another = "value2"
-        }
-      }
-
-      noncurrent_version_transition = [
-        {
-          days          = 30
-          storage_class = "STANDARD_IA"
-        },
-      ]
-
-      noncurrent_version_expiration = {
-        days = 300
-      }
-    },
-  ]
-
-  intelligent_tiering = {
-    general = {
-      status = "Enabled"
-      filter = {
-        prefix = "/"
-        tags = {
-          Environment = "dev"
-        }
-      }
-      tiering = {
-        ARCHIVE_ACCESS = {
-          days = 180
-        }
-      }
-    },
-    documents = {
-      status = false
-      filter = {
-        prefix = "documents/"
-      }
-      tiering = {
-        ARCHIVE_ACCESS = {
-          days = 125
-        }
-        DEEP_ARCHIVE_ACCESS = {
-          days = 200
-        }
-      }
-    }
-  }
-
-  metric_configuration = [
-    {
-      name = "documents"
-      filter = {
-        prefix = "documents/"
-        tags = {
-          priority = "high"
-        }
-      }
-    },
-    {
-      name = "other"
-      filter = {
-        tags = {
-          production = "true"
-        }
-      }
-    },
-    {
-      name = "all"
-    }
-  ]
 }
