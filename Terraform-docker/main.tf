7e8b07b13aab590c6b6cc4562d1f5d0e074173ba
Patryk-Kazmierczak <patrykkazmierczak0@gmail.com>
2023-04-14 11:32:08 +0200

    update

--- Terraform-docker/main.tf
+++ Terraform-docker/main.tf
@@ -1,4 +1,14 @@
 
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "3.0.2"
+    }
+  }
+}
+
+provider docker {}
 
 #-------------------------------------------------------------Terraform-Docker-Images----------------------------------------------------------------
 
