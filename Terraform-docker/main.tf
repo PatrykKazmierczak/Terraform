483a786e2473d932ee52c326c7c57b8a0242ab31
PatrykKazmierczak <patrykkazmierczak0@gmail.com>
2023-04-16 17:08:07 +0200

    update s3 buckets

--- Terraform-docker/main.tf
+++ Terraform-docker/main.tf
@@ -16,14 +16,116 @@ provider "aws" {
   
 }
 
-
-resource "aws_s3_bucket" "aws_s3_bucket" {
-  bucket = "aws-s3-bucket-pkaz-devops"
+resource "aws_s3_bucket" "aws_s3_bucket_1" {
+  bucket = "aws-s3-bucket-pkaz-devops-1"
 
   tags = {
-    Name        = "My bucket"
+    Name        = "My bucket-1"
     Environment = "Dev"
   }
+
+  versioning {
+    enabled = true
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_s3_bucket" "aws_s3_bucket_2" {
+  bucket = "aws-s3-bucket-pkaz-devops-2"
+
+  tags = {
+    Name        = "My bucket-2"
+    Environment = "Dev"
+  }
+
+  versioning {
+    enabled = true
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_s3_bucket" "aws_s3_bucket_3" {
+  bucket = "aws-s3-bucket-pkaz-devops-3"
+
+  tags = {
+    Name        = "My bucket-3"
+    Environment = "Dev"
+  }
+
+  versioning {
+    enabled = true
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_s3_bucket_policy" "aws_s3_bucket_policy_1" {
+  bucket = aws_s3_bucket.aws_s3_bucket_1.id
+
+  policy = jsonencode({
+    "Id": "Policy1681656626372",
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Sid": "Stmt1681656625569",
+        "Action": [
+          "s3:GetObject"
+        ],
+        "Effect": "Allow",
+        "Resource": "arn:aws:s3:::aws-s3-bucket-pkaz-devops-1/*",
+        "Principal": "*"
+      }
+    ]
+  })
+}
+
+
+resource "aws_s3_bucket_policy" "aws_s3_bucket_policy_2" {
+  bucket = aws_s3_bucket.aws_s3_bucket_2.id
+
+  policy = jsonencode({
+    "Id": "Policy1681656626372",
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Sid": "Stmt1681656625569",
+        "Action": [
+          "s3:GetObject"
+        ],
+        "Effect": "Allow",
+        "Resource": "arn:aws:s3:::aws-s3-bucket-pkaz-devops-2/*",
+        "Principal": "*"
+      }
+    ]
+  })
+}
+
+resource "aws_s3_bucket_policy" "aws_s3_bucket_policy_3" {
+  bucket = aws_s3_bucket.aws_s3_bucket_3.id
+
+  policy = jsonencode({
+    "Id": "Policy1681656626372",
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Sid": "Stmt1681656625569",
+        "Action": [
+          "s3:GetObject"
+        ],
+        "Effect": "Allow",
+        "Resource": "arn:aws:s3:::aws-s3-bucket-pkaz-devops-3/*",
+        "Principal": "*"
+      }
+    ]
+  })
 }
 
 #-------------------------------------------------------------Terraform-Docker----------------------------------------------------------------
