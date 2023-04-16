acb55a9f19dda9e72ee10a8f1a2c7982fe918046
PatrykKazmierczak <patrykkazmierczak0@gmail.com>
2023-04-16 17:35:00 +0200

    update structure

--- Terraform-docker/main.tf
+++ Terraform-docker/main.tf
@@ -141,33 +141,6 @@ terraform {
 
 
 provider docker {}
-#-------------------------------------------------------------Terraform-Docker-Variables----------------------------------------------------------------
-
-variable "ext_port" {
-  type = number
-  default = 1880
-
-  validation {
-      condition = var.ext_port <= 65535 && var.ext_port >0
-      error_message = "Rhe external port must be in the valid port range 0 - 65535"
-  }
-}
-
-variable "int_port" {
-  type = number
-  default = 1880
-
-  validation {
-      condition = var.int_port == 1880
-      error_message = "The internal port must be 1880"
-  }
-  
-}
-
-variable "cont_count" {
-  type = number
-  default = 1
-}
 
 #-------------------------------------------------------------Terraform-Docker-Images----------------------------------------------------------------
 
@@ -274,39 +247,9 @@ resource "docker_container" "jenkins_container" {
   
 # }
 
-#----------------------------------------------------------------Terrform-Output----------------------------------------------------------------
 
-output "ip-address" {
-  value = join("\n ",  flatten([docker_container.nodered_container[*].name, 
-                                docker_container.nodered_container[*].network_data[*].ip_address,
-                                docker_container.nodered_container[*].ports[*].external]))
-  description = "The IP address and external port of the container"
-}
 
-output "ip-address-and-ports-nodered" {
-  value = [for i in docker_container.nodered_container[*]: join(":",i.network_data[*]["ip_address"], i.ports[*]["external"], i.name[*])]
-  description = "The IP address and external port of the container"
-}
 
-output "ip-address-and-ports-ubuntu" {
-  value = [for i in docker_container.ubuntu_container[*]: join(":",i.network_data[*]["ip_address"], i.ports[*]["external"], i.name[*])]
-  description = "The IP address and external port of the container"
-}
-
-output "ip-address-and-ports-jenkins" {
-  value = [for i in docker_container.jenkins_container[*]: join(":",i.network_data[*]["ip_address"], i.ports[*]["external"], i.name[*])]
-  description = "The IP address and external port of the container"
-}
-
-output "ip-address-and-ports-debian" {
-  value = [for i in docker_container.debian_container[*]: join(":",i.network_data[*]["ip_address"], i.ports[*]["external"], i.name[*], i.network_data[*]["mac_address"])]
-  description = "The IP address and external port of the container"
-}
-
-output "ip-address-and-ports-postgresql" {
-  value = [for i in docker_container.postgresql_container[*]: join(":",i.network_data[*]["ip_address"], i.ports[*]["external"], i.name[*], i.network_data[*]["mac_address"])]
-  description = "The IP address and external port of the container"
-}
 
 
 
---
+++ Terraform-docker/outputs.tf
@@ -0,0 +1,31 @@
+output "ip-address" {
+  value = join("\n ",  flatten([docker_container.nodered_container[*].name, 
+                                docker_container.nodered_container[*].network_data[*].ip_address,
+                                docker_container.nodered_container[*].ports[*].external]))
+  description = "The IP address and external port of the container"
+}
+
+output "ip-address-and-ports-nodered" {
+  value = [for i in docker_container.nodered_container[*]: join(":",i.network_data[*]["ip_address"], i.ports[*]["external"], i.name[*])]
+  description = "The IP address and external port of the container"
+}
+
+output "ip-address-and-ports-ubuntu" {
+  value = [for i in docker_container.ubuntu_container[*]: join(":",i.network_data[*]["ip_address"], i.ports[*]["external"], i.name[*])]
+  description = "The IP address and external port of the container"
+}
+
+output "ip-address-and-ports-jenkins" {
+  value = [for i in docker_container.jenkins_container[*]: join(":",i.network_data[*]["ip_address"], i.ports[*]["external"], i.name[*])]
+  description = "The IP address and external port of the container"
+}
+
+output "ip-address-and-ports-debian" {
+  value = [for i in docker_container.debian_container[*]: join(":",i.network_data[*]["ip_address"], i.ports[*]["external"], i.name[*], i.network_data[*]["mac_address"])]
+  description = "The IP address and external port of the container"
+}
+
+output "ip-address-and-ports-postgresql" {
+  value = [for i in docker_container.postgresql_container[*]: join(":",i.network_data[*]["ip_address"], i.ports[*]["external"], i.name[*], i.network_data[*]["mac_address"])]
+  description = "The IP address and external port of the container"
+}
