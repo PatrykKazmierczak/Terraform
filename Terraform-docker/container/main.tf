f0d3dc39a920b056c15733f8139d5435ab78abfb
PatrykKazmierczak <patrykkazmiercza0@gmail.com>
2023-05-01 19:32:36 +0000

    update

--- Terraform-docker/container/main.tf
+++ Terraform-docker/container/main.tf
@@ -7,7 +7,7 @@ resource "random_string" "random" {
 
 resource "docker_container" "app_container" {
   count = var.count_in
-  name  = join("-", [var.name_in, terraform.workspace, random_string.random[count.index].result])
+  name  = "${var.name_in}-${terraform.workspace}-${ random_string.random[count.index].result}"
   image = var.image_in
 
   ports {
@@ -19,42 +19,17 @@ resource "docker_container" "app_container" {
     for_each = var.volumes_in
     content {
     container_path = volumes.value["container_path_each"]
-    volume_name    = docker_volume.container_volume[count.index].name
+    volume_name    = module.volume[count.index].volume.output[volume.key]
     }
   }
-
-  # provisioner "local-exec"{
-  # command = "echo ${self.name}: ${self.ip_address}:${join("", [for x in self.ports[*]["external"]: x])} >> containers.txt"
-  # }
-
-  provisioner "local-exec" {
-  when = destroy
-  command = "rm -f containers.txt"
-  }
-
-  provisioner "local-exec" {
-  when = destroy
-  command = "mkdir ${path.cwd}/../backup/"
-  on_failure = continue
-
-  }
-  provisioner "local-exec" {
-  when = destroy
-  command = "sudo tar -czvf ${path.cwd}/../backup/${self.name}.tar.gz"
-  on_failure = fail
+}
+  module "volume" {
+    source = "./volume"
+    count = var.count_in
+    volume_count = length(var.volumes_in)
+    volume_name = "${var.name_in}-${terraform.workspace}-${random_string.random[count.index].result}-volume"
   }
   
-}
-
-resource "docker_volume" "container_volume" {
-  count = length(var.volumes_in)
-  name  = "${var.name_in}-${count.index}-volume"
-  lifecycle {
-    prevent_destroy = false
-  }
-}
-
-
 
 
 
--- Terraform-docker/locals.tf
+++ Terraform-docker/locals.tf
@@ -17,9 +17,9 @@ locals {
     #   int = 7070
     #   ext = var.ext_port["ubuntu"][terraform.workspace]
     #   volumes = [
-      #   {container_path_each = "/var/lib/ubuntu"},
-      #   {container_path_each = "/etc/ubuntu"}
-      # ]
+    #     {container_path_each = "/var/lib/ubuntu"},
+    #     {container_path_each = "/etc/ubuntu"}
+    #   ]
     # }
     
     grafana = {
@@ -49,10 +49,10 @@ locals {
       image = var.image["prometheus"][terraform.workspace]
       int = 9090
       ext = var.ext_port["prometheus"][terraform.workspace]
-      # volumes = [
-      #   {container_path_each = "/var/lib/prometheus"},
-      #   {container_path_each = "/etc/prometheus"}
-      # ]
+      volumes = [
+        {container_path_each = "/var/lib/prometheus"},
+        {container_path_each = "/etc/prometheus"}
+      ]
     }
 
     portainer = {
