ab8cb68dd31e500a04ebab08b9331dd15fe529da
PatrykKazmierczak <patrykkazmierczak0@gmail.com>
2023-07-10 19:13:42 +0200

    update

--- Terraform-aws-udemy/networking/main.tf
+++ Terraform-aws-udemy/networking/main.tf
@@ -12,3 +12,15 @@ resource "aws_vpc" "dev_vpc" {
         Name = "dev_vpc-${random_integer.random.id}"
     }
 }
+
+resource "aws_subnet" "dev_public_subnet" {
+    count = length(var.public_cidrs)
+    vpc_id = aws_vpc.dev_vpc.id
+    cidr_block = var.public_cidrs[count.index]
+    map_public_ip_on_launch = true
+    availability_zone = ["eu-central-1a", "eu-central-1b", "eu-central-1c"][count.index]
+
+    tags = {
+        Name = "dev_public_${count.index + 1}"
+    }
+}
